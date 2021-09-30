c7cb23fafdb67a4e5413e2f96a54983832cdb4ab
stanley1206 <stanley2622@gmail.com>
2021-09-30 20:35:50 +0800

    first commit

---
+++ src/App_04.js
@@ -0,0 +1,15 @@
+import Todo_04 from './components/Todo_04';
+import './App.css';
+
+function App_04() {
+  return (
+    <div >
+      <h1>My Todos</h1>
+      <Todo_04 text='Learn React' />
+      <Todo_04 text='Learn Abc' />
+      <Todo_04 text='Learn Def' />
+    </div >
+  );
+}
+
+export default App_04;
---
+++ src/components/Todo_04.js
@@ -0,0 +1,18 @@
+function Todo_04(props) {
+    return (
+        <div >
+
+            <div className="card">
+                <h2>{props.text}
+
+
+                </h2>
+                <div className="actions">
+                    <button className="btn">Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Todo_04;

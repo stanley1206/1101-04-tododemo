6191f028f97f1162f3600d24564a2d50e994c2f9
stanley1206 <stanley2622@gmail.com>
2021-10-07 20:59:35 +0800

    add <Backdrop_04 />, <Modal_04 />

---
+++ src/components/Backdrop_04.js
@@ -0,0 +1,9 @@
+function Backdrop_04(props) {
+    return (
+        <div class="backdrop" onClick={props.onClose}>
+
+        </div>
+    )
+}
+
+export default Backdrop_04;
--- src/components/Todo_04.js
+++ src/components/Todo_04.js
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+import Modal_04 from '../components/Modal_04';
+import Backdrop_04 from '../components/Backdrop_04'
+
 function Todo_04(props) {
+    const [showModal, setShowModal] = useState();
+
+    function showModalHandler() {
+        setShowModal(true)
+    }
+
+    function closeModalHandler() {
+        setShowModal(false)
+    }
+
     return (
         <div >
-
             <div className="card">
-                <h2>{props.text}
-
-
-                </h2>
+                <h2>{props.text}</h2>
                 <div className="actions">
-                    <button className="btn">Delete</button>
+                    <button className="btn" onClick={showModalHandler}>Delete</button>
                 </div>
             </div>
+            {showModal && <Backdrop_04 onClose={closeModalHandler} />}
+            {showModal && <Modal_04 text='Are you sure?' onClose={closeModalHandler} />}
         </div>
     );
 }
